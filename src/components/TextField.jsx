7b403aec77c6cc3846d2294f5e5b1115fe0f0a8e
ashleymays <ashleymays091@gmail.com>
2023-11-26 23:03:39 -0800

    Use useRef instead of useState for content editable

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 import ContentEditable from "react-contenteditable";
 
 function TextField() {
-  const [html, setHtml] = useState("");
+  const text = useRef('');
+
+  const handleChange = (event) => {
+    text.current = event.target.value;
+  }
 
   return (
     <ContentEditable
-      html={html}
+      html={text.current}
       className="p-4 lg:px-80 lg:py-20 2xl:px-80 2xl:py-20 text-lg inline-block leading-normal w-full min-h-full whitespace-pre-wrap break-words outline-none empty:before:pointer-events-none empty:before:content-[attr(placeholder)] empty:before:text-gray-400"
       placeholder="Once upon a time..."
-      onChange={(event) => setHtml(event.target.value)}
+      onChange={handleChange}
     />
   );
 }
