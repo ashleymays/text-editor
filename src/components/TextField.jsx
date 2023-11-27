ab454d072d544cf0ddf0e28af3787b62e2ab9825
ashleymays <ashleymays091@gmail.com>
2023-11-26 23:15:55 -0800

    Use localstorage instead of useRef in content editable

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,19 +1,16 @@
-import { useRef } from 'react';
 import ContentEditable from "react-contenteditable";
 
 function TextField() {
-  const text = useRef('');
-
-  const handleChange = (event) => {
-    text.current = event.target.value;
+  const updateText = (event) => {
+    window.localStorage.setItem('text', event.target.value);
   }
 
   return (
     <ContentEditable
-      html={text.current}
+      html={window.localStorage.getItem('text')}
       className="p-4 lg:px-80 lg:py-20 2xl:px-80 2xl:py-20 text-lg inline-block leading-normal w-full min-h-full whitespace-pre-wrap break-words outline-none empty:before:pointer-events-none empty:before:content-[attr(placeholder)] empty:before:text-gray-400"
       placeholder="Once upon a time..."
-      onChange={handleChange}
+      onChange={updateText}
     />
   );
 }
