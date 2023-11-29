ec545e428c98ecbf86f5b23d16439d2c3e8fade8
ashleymays <ashleymays091@gmail.com>
2023-11-28 18:19:46 -0800

    Use Tailwind fonts

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -1,6 +1,6 @@
-function Option({ children, icon }) {
+function Option({ children, icon, onClick }) {
   return (
-    <button type="button" alt={children} className="relative after:absolute after:top-1/2 after:right-9 after:translate-y-neg50 after:content-[attr(alt)] after:text-base after:hidden hover:opacity-50 sm:hover:opacity-50 md:hover:opacity-50 lg:hover:opacity-100 xl:hover:opacity-100 2xl:hover:opacity-100 lg:hover:after:block xl:hover:after:block 2xl:hover:after:block">
+    <button type="button" alt={children} onClick={onClick} className="font-sans relative after:absolute after:top-1/2 after:right-9 after:translate-y-neg50 after:content-[attr(alt)] after:text-sm after:tracking-wide after:hidden hover:opacity-50 sm:hover:opacity-50 md:hover:opacity-50 lg:hover:opacity-100 xl:hover:opacity-100 2xl:hover:opacity-100 lg:hover:after:block xl:hover:after:block 2xl:hover:after:block">
       {icon}
     </button>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,20 +4,26 @@ import Option from './Option';
 const options = [
   {
     icon: <Maximize size={20} />,
-    title: 'Fullscreen'
+    title: 'Fullscreen',
+    onClick: () => {
+      console.log('Fullscreen clicked')
+    }
   },
   {
     icon: <Download size={20} />,
-    title: 'Download'
+    title: 'Download',
+    onClick: () => {
+      console.log('Download clicked')
+    }
   }
 ];
 
 function Sidebar() {
-  const optionsList = options.map(({ title, icon }) => (
-    <Option icon={icon}>{title}</Option>
+  const optionsList = options.map((option) => (
+    <Option key={option.title} {...option}>{option.title}</Option>
   ));
   return (
-    <aside className="fixed top-1/2 right-0 px-3.5 flex flex-col items-end gap-y-6">
+    <aside className="fixed top-1/2 right-0 px-3.5 flex flex-col items-end gap-y-7">
       {optionsList}
     </aside>
   );
