fd33bbd5e17cd12570e746db164057d83b43de49
Ashley Mays <82427430+ashleymays@users.noreply.github.com>
2023-12-01 03:54:05 +0000

    Add favicon and adjust text colors

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,37 +1,17 @@
-import { useState, useEffect } from 'react';
-
 function TextField() {
-  const [text, setText] = useState('');
-
-  const handleOnChange = (event) => {
-    setText(event.target.value);
+  const handleChange = (event) => {
+    window.localStorage.setItem('innerText', event.target.value);
   };
-
-  const handlePaste = (event) => {
-    event.preventDefault();
-    const originalText = window.localStorage.getItem('innerText');
-    const newText = event.clipboardData.getData('text/plain');
-    setText(originalText + newText);
-  };
-
-  useEffect(() => {
-    setText(window.localStorage.getItem('innerText'));
-  }, []);
-
-  useEffect(() => {
-    window.localStorage.setItem('innerText', text);
-  }, [text]);
 
   return (
     <div
       contentEditable={true}
       placeholder="Once upon a time..."
-      className="font-mono text-gray-900 p-4 lg:px-80 lg:py-20 xl:px-80 xl:py-20 2xl:px-80 2xl:py-20 text-base inline-block leading-7 w-full min-h-screen whitespace-pre-wrap break-words outline-none empty:before:pointer-events-none empty:before:content-[attr(placeholder)] empty:before:text-gray-400"
-      onChange={handleOnChange}
-      onPaste={handlePaste}
+      className="font-mono text-gray-900 p-4 lg:px-80 lg:py-16 xl:px-80 xl:py-16 2xl:px-80 2xl:py-16 text-base inline-block leading-10 w-full min-h-screen whitespace-pre-wrap break-words outline-none empty:before:pointer-events-none empty:before:content-[attr(placeholder)] empty:before:text-gray-500"
+      onChange={handleChange}
       spellCheck={false}
     >
-      {text}
+      {window.localStorage.getItem('innerText')}
     </div>
   );
 }
