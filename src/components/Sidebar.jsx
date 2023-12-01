d6e43e71b033af3d104764c8a6161b0d47d07591
Ashley Mays <82427430+ashleymays@users.noreply.github.com>
2023-12-01 06:22:55 +0000

    Move options array outside of component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,20 +2,20 @@ import { Download, Maximize } from 'react-feather';
 import Option from './Option';
 import { toggleFullscreen, downloadFile } from '../lib/optionHandlers';
 
-function Sidebar() {
-  const options = [
-    {
-      title: 'Fullscreen',
-      icon: <Maximize size={18} />,
-      onClick: toggleFullscreen
-    },
-    {
-      title: 'Download',
-      icon: <Download size={18} />,
-      onClick: downloadFile
-    }
-  ];
+const options = [
+  {
+    title: 'Fullscreen',
+    icon: <Maximize size={18} />,
+    onClick: toggleFullscreen
+  },
+  {
+    title: 'Download',
+    icon: <Download size={18} />,
+    onClick: downloadFile
+  }
+];
 
+function Sidebar() {
   return (
     <aside className="sticky top-0 h-screen flex flex-col gap-y-7 px-4 py-6 lg:fixed xl:fixed 2xl:fixed lg:right-0 xl:right-0 2xl:right-0 lg:px-4 lg:py-16 xl:px-4 xl:py-16 2xl:px-4 2xl:py-16">
       {options.map((option) => (
