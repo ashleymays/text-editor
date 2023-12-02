ec6b628899efc908aa794650a51636ea543aecbf
Ashley Mays <82427430+ashleymays@users.noreply.github.com>
2023-12-02 18:23:26 +0000

    Fix textfield padding and spacing

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -1,3 +1,4 @@
+import TextareaAutosize from 'react-textarea-autosize';
 /**
  * Light Mode:
  *  - text: text-neutral-900
@@ -10,24 +11,14 @@
  *  - hover: text-neutral-700
  */
 function TextField() {
-  const handleChange = (event) => {
-    window.localStorage.setItem('text', event.target.value);
-  };
-
-  const handlePaste = (event) => {
-    event.preventDefault();
-  };
-
   return (
-    <div
-      contentEditable={true}
-      placeholder="Once upon a time..."
-      className="sm:mr-12 md:mr-12 pb-16 p-4 pr-0 lg:px-80 lg:pt-16 xl:px-80 xl:pt-16 2xl:px-80 2xl:pt-16 font-mono text-neutral-400 text-base tracking-tight leading-10 whitespace-pre-wrap break-words outline-none empty:before:pointer-events-none empty:before:content-[attr(placeholder)] empty:before:text-neutral-700"
-      onChange={handleChange}
-      onPaste={handlePaste}
-      spellCheck={false}
-    >
-      {window.localStorage.getItem('text') || ''}
+    <div className="flex w-full h-full">
+      <TextareaAutosize
+        placeholder="Once upon a time..."
+        className="font-mono text-neutral-400 text-base tracking-tight leading-10 placeholder:text-neutral-700 flex-1 resize-none h-full border-none outline-none overflow-hidden bg-transparent mr-12 pb-48 pt-4 pl-4 pr-0 lg:px-80 lg:pt-16 xl:px-80 xl:pt-16 2xl:px-80 2xl:pt-16"
+        spellCheck={false}
+        autoFocus
+      />
     </div>
   );
 }
