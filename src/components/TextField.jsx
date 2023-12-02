ceccc1243fc715e6858cd93b3c5237ff083eed25
Ashley Mays <82427430+ashleymays@users.noreply.github.com>
2023-12-02 21:32:46 +0000

    Set minimum height on text field to 100%

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -18,7 +18,7 @@ function TextField() {
     <div className="flex w-full h-full">
       <TextareaAutosize
         placeholder="Once upon a time..."
-        className="font-mono text-neutral-400 text-base tracking-tight leading-10 placeholder:text-neutral-700 flex-1 resize-none h-full border-none outline-none overflow-hidden bg-transparent mr-12 pb-48 pt-4 pl-4 pr-0 lg:px-80 lg:pt-16 xl:px-80 xl:pt-16 2xl:px-80 2xl:pt-16"
+        className="border-none outline-none min-h-full font-mono text-neutral-400 text-base tracking-tight leading-10 placeholder:text-neutral-700 flex-1 resize-none overflow-hidden bg-transparent mr-12 pb-48 pt-4 pl-4 pr-0 lg:px-80 lg:pt-16 xl:px-80 xl:pt-16 2xl:px-80 2xl:pt-16"
         spellCheck={false}
         onInput={(event) => setText(event.target.value)}
         value={text}
