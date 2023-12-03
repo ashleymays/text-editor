c6c06f1824d9433d81f1375f9c6d84048092dd7f
Ashley Mays <82427430+ashleymays@users.noreply.github.com>
2023-12-03 03:16:01 +0000

    Add color scheme setting by browser choice

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -8,7 +8,7 @@ function TextField() {
     <div className="flex w-full h-full">
       <TextareaAutosize
         placeholder="Once upon a time..."
-        className="border-none outline-none min-h-full font-mono text-neutral-400 text-base tracking-tight leading-10 placeholder:text-neutral-700 flex-1 resize-none overflow-hidden bg-transparent mr-12 pb-48 pt-4 pl-4 pr-0 lg:px-80 lg:pt-16 xl:px-80 xl:pt-16 2xl:px-80 2xl:pt-16"
+        className="text-neutral-900 placeholder:text-neutral-400 dark:text-zinc-400 dark:placeholder:text-zinc-800 border-none outline-none min-h-full font-mono text-base tracking-tight leading-10 flex-1 resize-none overflow-hidden bg-transparent mr-12 pb-48 pt-4 pl-4 pr-0 lg:px-80 lg:pt-16 xl:px-80 xl:pt-16 2xl:px-80 2xl:pt-16"
         spellCheck={false}
         onInput={(event) => setText(event.target.value)}
         value={text}
