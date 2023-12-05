8e2cf75435eb069997759f8ca3759ca07ce6a077
Ashley Mays <82427430+ashleymays@users.noreply.github.com>
2023-12-05 04:19:05 +0000

    Adjust colors and change favicon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,12 +5,12 @@ import { toggleFullscreen, downloadTextFile } from '../lib/sidebarItemHandlers';
 const sidebarItemIconSize = 20;
 const sidebarItems = [
   {
-    title: 'Fullscreen',
+    title: 'fullscreen',
     icon: <CornersOut size={sidebarItemIconSize} />,
     onClick: toggleFullscreen
   },
   {
-    title: 'Download',
+    title: 'download',
     icon: <DownloadSimple size={sidebarItemIconSize} />,
     onClick: downloadTextFile
   }
@@ -18,7 +18,7 @@ const sidebarItems = [
 
 function Sidebar() {
   return (
-    <aside className="fixed top-0 bottom-0 right-0 w-10 md:w-12 lg:w-12 xl:w-14 2xl:w-14 h-full flex flex-col items-center py-6 md:py-16 lg:py-16 xl:py-16 2xl:py-16 gap-y-5">
+    <aside className="fixed top-0 bottom-0 left-0 w-10 md:w-12 lg:w-12 xl:w-14 2xl:w-14 h-full flex flex-col items-center py-6 md:py-20 lg:py-20 xl:py-20 2xl:py-20 gap-y-5">
       {sidebarItems.map((item) => (
         <SidebarItem key={item.title} {...item}>
           {item.icon}
--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -16,7 +16,7 @@ function TextField() {
     <div className="flex w-full h-full">
       <TextareaAutosize
         placeholder="Once upon a time..."
-        className="min-h-full border-none outline-none text-neutral-900 placeholder:text-neutral-400 dark:text-zinc-600 dark:placeholder:text-zinc-900 font-mono text-base leading-8 flex-1 resize-none overflow-hidden bg-transparent mr-10 md:mr-12 lg:mr-0 xl:mr-0 2xl:mr-0 pb-72 pt-4 pl-4 pr-0 md:px-28 md:pt-16 lg:px-60 lg:pt-16 xl:px-80 xl:pt-16 2xl:px-80 2xl:pt-16"
+        className="min-h-full border-none outline-none text-neutral-900 placeholder:text-neutral-400 dark:text-neutral-300 dark:placeholder:text-neutral-600 font-mono text-base max-xs:text-xs max-sm:text-xs leading-loose flex-1 resize-none overflow-hidden bg-transparent ml-10 md:ml-12 lg:ml-0 xl:ml-0 2xl:ml-0 pb-72 pt-4 pl-0 pr-4 md:px-28 md:pt-20 lg:px-60 lg:pt-20 xl:px-80 xl:pt-20 2xl:px-80 2xl:pt-20"
         spellCheck={false}
         onInput={updateText}
         onHeightChange={scrollToBottom}
