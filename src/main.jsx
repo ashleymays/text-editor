8a15762143d2c5b6aaa47354a3f622ed5e29bf20
Ashley Mays <82427430+ashleymays@users.noreply.github.com>
2023-12-09 19:19:38 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { Analytics } from '@vercel/analytics/react';
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { inject } from '@vercel/analytics';
 import App from './app';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Analytics mode={'production'} />
   </React.StrictMode>
 );
+
+inject();
