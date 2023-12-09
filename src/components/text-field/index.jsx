4a1cd7f10d65a8881defaa93dc21879f7d22d93a
Ashley Mays <82427430+ashleymays@users.noreply.github.com>
2023-12-09 04:48:30 +0000

    Update index.jsx

--- src/components/text-field/index.jsx
+++ src/components/text-field/index.jsx
@@ -1,6 +1,12 @@
 import TextareaAutosize from 'react-textarea-autosize';
 import useLocalStorage from '../../hooks/use-local-storage';
 
+const placeholder = `Minimal Text Editor is designed to emulate the simplicity of a typewriter.\n
+It automatically saves your work, so you never lose a single sentence even if you close the tab.\n
+Use the sidebar to adjust your settings and download your work.\n
+Now start writing...
+`;
+
 function TextField() {
   const [text, setText] = useLocalStorage('text', '');
 
@@ -15,7 +21,7 @@ function TextField() {
   return (
     <div className="flex w-full h-full">
       <TextareaAutosize
-        placeholder="Once upon a time..."
+        placeholder={placeholder}
         className="min-h-full border-none outline-none text-neutral-900 placeholder:text-neutral-400 dark:text-neutral-300 dark:placeholder:text-neutral-600 font-mono text-base max-xs:text-xs max-sm:text-xs leading-loose flex-1 resize-none overflow-hidden bg-transparent ml-10 md:ml-12 lg:ml-0 xl:ml-0 2xl:ml-0 pb-[45vh] pt-4 pl-0 pr-4 md:px-28 md:pt-20 lg:px-60 lg:pt-20 xl:px-80 xl:pt-20 2xl:px-80 2xl:pt-20"
         spellCheck={false}
         onInput={updateText}
